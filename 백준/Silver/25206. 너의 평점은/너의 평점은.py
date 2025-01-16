cc5281382ac2421804af41255cd91f868a287b49
JEONG DO HEON <weg1456@gmail.com>
2025-01-16 22:06:26 +0900

    Update 너의 평점은.py
    바꿈

--- 백준/Silver/25206. 너의 평점은/너의 평점은.py
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -1,8 +1,6 @@
-score = {'A+': 4.5, 'A0': 4.0, 'B+': 3.5, 
-         'B0': 3.0, 'C+': 2.5, 'C0': 2.0,
-         'D+': 1.5, 'D0': 1.0, 'F':0.0}
+score = {'A+': 4.5, 'A0': 4.0, 'B+': 3.5,'B0': 3.0, 'C+': 2.5, 'C0': 2.0,'D+': 1.5, 'D0': 1.0, 'F':0.0}
 average = 0
-totall = 0
+total = 0
 
 for i in range(20):
   lecture, credit, grade = input().split() 
@@ -10,6 +8,6 @@ for i in range(20):
     continue
   else:
     average += float(credit)*score[grade]
-    totall +=float(credit)
+    total +=float(credit)
     
-print(average/totall)
+print(average/total)
