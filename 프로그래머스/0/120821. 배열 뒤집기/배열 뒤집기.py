e362aeb30a4d7a9603e43d364dcaaa93ad7bb7cb
JEONG DO HEON <weg1456@gmail.com>
2025-01-17 22:22:58 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.py
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.py
@@ -1,6 +1,3 @@
 def solution(num_list):
-    answer = []
-    for i in range(len(num_list)):
-        k = len(num_list) - (i+1)
-        answer.append(num_list[k])
-    return answer
+    num_list.reverse()
+    return num_list
