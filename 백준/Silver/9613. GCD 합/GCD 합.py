1dda43deb7e0d16b9eca458a982de56ca6e44e4a
JEONG DO HEON <weg1456@gmail.com>
2025-02-09 19:54:45 +0900

    [Silver IV] Title: GCD 합, Time: 188 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Silver/9613. GCD 합/GCD 합.py
+++ 백준/Silver/9613. GCD 합/GCD 합.py
@@ -1,14 +1,10 @@
 import sys
-def gcd(a,b):
-  while b > 0:
-      a , b = b, a%b
-  return a
-
+import math
 N = int(input())
 for _ in range(N):
     a = list(map(int, sys.stdin.readline().split()))
     b = 0
     for i in range(1,len(a)):
       for j in range(1+i,len(a)):
-        b += gcd(a[i],a[j])
+        b += math.gcd(a[i],a[j])
     print(b)
