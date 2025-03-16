bf1039b46887888367481e877a660f9b8af2ad7f
JEONG DO HEON <weg1456@gmail.com>
2025-03-16 20:55:53 +0900

    [Silver I] Title: 정수 삼각형, Time: 80 ms, Memory: 36504 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -1,18 +1,13 @@
 def solution(a):
-    b = a   
-    for i in range(1,N):
-        for j in range(0,i+1):
-            if j == 0:
-                b[i][j] += b[i-1][j]
-            elif j == i:
-                b[i][j] += b[i-1][j-1]
-            else:
-                b[i][j] += max(b[i-1][j-1],b[i-1][j])
-    return max(b[N-1])
+    b = a[-1]
+    for i in range(len(a)-2,-1,-1):
+        for j in range(len(a[i])):
+            b[j] = a[i][j] + max(b[j],b[j+1])
+    return b[0]
 
 
 N = int(input())
-a = []
+a = [[]]*N
 for i in range(N):
-    a.append(list(map(int, input().split())))
+    a[i] = list(map(int, input().split()))
 print(solution(a))
