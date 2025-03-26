24399253e2e3c4141be4c7320a06e04c5fee81f2
JEONG DO HEON <weg1456@gmail.com>
2025-03-26 15:37:57 +0900

    [Silver V] Title: 숫자 카드, Time: 3676 ms, Memory: 108796 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -0,0 +1,23 @@
+N = int(input())
+A = list(map(int, input().split()))
+M = int(input())
+B = list(map(int, input().split()))
+A.sort()
+
+for i in B:
+    First, Last = 0, N-1
+    Find = False
+
+    while First <= Last:
+        Mid = (First + Last)//2
+        if i == A[Mid]:
+              Find = True
+              print(1, end=' ')
+              break
+        elif i > A[Mid]:
+             First = Mid + 1
+        else:
+             Last = Mid - 1
+    
+    if not Find:
+         print(0, end=' ')
